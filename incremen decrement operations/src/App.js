cbb7bd6be669a7029c27fbaebbf44bc2d495b2e8
MUHAMMAD UMAIR <84286269+MuhammadUmair2008@users.noreply.github.com>
2022-01-23 22:03:00 +0500

    IDK

---
+++ incremen decrement operations/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./App.css";
+import {incNumber} from "./actions";
+import {decNumber} from "./actions";
+
+import { useSelector, useDispatch } from "react-redux";
+
+const App = () => {
+
+  // it alternative to the useContext hooks in react / consumer from context API
+  const changeTheNumber = useSelector(state => state.changeTheNumber);
+  
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div className="main-div">
+      
+
+      <div class="container">
+  
+      <h1><u>INCREMENT/DECREMENT COUNTER</u></h1>
+      <br/><br/><br/> 
+      
+      <div class="quantity">
+        <a class="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber())}><span>-</span></a>
+        <input name="quantity" type="text" class="quantity__input" value={changeTheNumber} />
+        <a class="quantity__plus" title="Increment" onClick={() => dispatch(incNumber(5))}><span>+</span></a>
+      </div>
+   
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default App
